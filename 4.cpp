f70089e3dedb329e4c8730794c8f1c68f1b570fa
mcctnn <92088210+mcctnn@users.noreply.github.com>
2023-03-25 13:32:43 +0300

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	double* hafta = nullptr;
+	cout << "Diyet programinizin suresini giriniz:";
+	int n;
+	cin >> n;
+	hafta = new double[n];
+	for (int i = 0; i < n; i++)
+	{
+		cout << i + 1 << ". hafta kilonuz:";
+		cin >> hafta[i];
+	}
+	cout << "Bu diyet programı ile toplam "<<hafta[0]-hafta[n-1] << " kilo kaybettiniz";
+	delete[] hafta;
+	return 0;
+}
+
+
+
+
