3803aab67de9264e76a44aac6aa1206ecf4d348d
mcctnn <92088210+mcctnn@users.noreply.github.com>
2023-03-26 02:30:55 +0300

    Add files via upload

---
+++ Cozumler1/2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+/*
+* x->z
+* z->y
+* y->x gösterecek
+*/
+void degistir(int *x,int *y,int *z) 
+{
+    int _x, _y, _z;
+    _x = *x;
+    _y = *y;
+    _z = *z;
+    *z = _x;
+    *y = _z;
+    *x = _y;
+}
+int main()
+{
+    int x = 10, y = 20, z = 30;
+    degistir(&x,&y,&z);
+    cout << x << endl;
+    cout << y << endl;
+    cout << z << endl;
+}
---
+++ Cozumler1/5.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+void mod(int);
+int main()
+{
+    int *m;
+    cout << "Veri serisinin boyutunu giriniz: ";
+    int n; cin >> n;
+    m = new int[n];
+    if (n>9)
+    {
+        cout << "Iki basamaki sayi olamaz";
+        return 0;
+    }
+    cout << "Veri serisini giriniz: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> m[i];
+    }
+    cout << "Mod: ";
+    mod(n);
+    return 0;
+}
+void mod(int n)
+{
+    int bi = 0;
+    int *p;
+    p = new int[n];
+    int* modtut;
+    modtut = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        modtut[i] = 0;
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            if (p[i]==p[j])
+            {
+                modtut[i]++;
+            }
+        }
+        if (modtut[i]>modtut[bi])
+        {
+            bi = i;
+        }
+    }
+    cout << modtut[bi];
+}
