df7becd36e5f07cf9cf502ba396962950658b857
mcctnn <92088210+mcctnn@users.noreply.github.com>
2023-03-25 16:34:54 +0300

    Add files via upload

---
+++ 2.3.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+using namespace std;
+int n, o, d;
+class YerlesimBirimi {
+    string sehir;
+    int nufus, olumSayisi, dogumSayisi;
+   public: void isimAta();void nufusAta();void dogumAta();void olumAta();
+        void dogumOranBul();void olumOranBul(); 
+};
+
+int main()
+{
+    YerlesimBirimi yerlesimBirimi;
+    yerlesimBirimi.isimAta(); 
+    yerlesimBirimi.nufusAta();
+    yerlesimBirimi.dogumAta();
+    yerlesimBirimi.olumAta();
+    yerlesimBirimi.dogumOranBul();
+    yerlesimBirimi.olumOranBul();
+    return 0;
+}
+
+void YerlesimBirimi::isimAta()
+{
+    cout << "Yerlesim biriminin ismini giriniz:";
+    cin >> sehir;
+}
+
+void YerlesimBirimi::nufusAta()
+{
+    cout << "Nufusunu giriniz:";
+    cin >> nufus;
+
+}
+
+void YerlesimBirimi::dogumAta()
+{
+    cout << "Dogum sayisini giriniz:";
+    cin >> dogumSayisi;
+}
+
+void YerlesimBirimi::olumAta()
+{
+    cout << "Olum sayisini giriniz:";
+    cin >> olumSayisi;
+}
+void YerlesimBirimi::dogumOranBul()
+{
+    double dob = double(dogumSayisi) / nufus;
+    cout << sehir << "--- Dogum orani: " <<dob<< endl;
+}
+
+void YerlesimBirimi::olumOranBul()
+{
+    double oob = double(olumSayisi) / nufus;
+    cout <<sehir<<"--- Olum orani: "<<oob;
+}
