d7fca1732c8d71ae0f33ff1851cbc450be0586a9
mcctnn <92088210+mcctnn@users.noreply.github.com>
2023-03-26 02:34:47 +0300

    Add files via upload

---
+++ Cozumler2/2.2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+int h, r;
+class Silindir
+{
+int yukseklik, tabanYaricapi;
+public:
+    double yuzeyAlanıHesapla(); double hacimHesapla(); int pi = 3;
+};
+double Silindir::yuzeyAlanıHesapla()
+{
+    cout << "Yukseklik:";
+    cin >> h;
+    yukseklik = h;
+    cout << "Taban yaricapi:";
+    cin >> r;
+    tabanYaricapi = r;
+    return 2 * pi * r * h + 2 * pi * r;
+}
+double Silindir::hacimHesapla() 
+{
+    return pi * r *r * h;
+}
+int main()
+{
+    Silindir silindir;
+    cout << "Yuzey alanı:" << silindir.yuzeyAlanıHesapla() << endl;
+    cout << "Hacmi:" << silindir.hacimHesapla() << endl;
+    return 0;
+}
---
+++ Cozumler2/2.4.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+class KolaMakinesi
+{
+public:double kolaFiyat; int kolaSayi;
+    void ilkYukleme(double f=2.5,int s=100) 
+    {
+        kolaFiyat = f;
+        kolaSayi = s;
+    }
+    void kolaVerme() 
+    {
+        double para;
+        cout << "Kola fiyati: " << kolaFiyat << endl;
+        cout << "Odemeniz:";
+        cin >> para;
+        if (para>kolaFiyat)
+        {
+            cout << "Kolaniz veriliyor." << endl;
+            cout << "Para ustunuz " << para - kolaFiyat <<"TL" << endl;
+            kolaSayi--;
+        }
+    }
+    void goster() 
+    {
+        cout << "Son durum:" << endl;
+        cout << "    Kolanin fiyati: " << kolaFiyat << endl;
+        cout << "    Kola sayisi: " << kolaSayi << endl;
+    }
+};
+int main()
+{
+    KolaMakinesi kolaMakinesi;
+    kolaMakinesi.ilkYukleme();
+    kolaMakinesi.kolaVerme();
+    kolaMakinesi.goster();
+    return 0;
+}
